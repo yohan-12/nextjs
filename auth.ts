e0390ea1b24366dd81ed3d97aaa38533dd2ad6a7
yohan-12 <130564569+yohan-12@users.noreply.github.com>
2024-01-10 23:19:54 +0900

    Landing page

--- auth.ts
+++ auth.ts
@@ -1,8 +1,41 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
 export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,//rules we prepared earlier for who's allowed or not
-  providers: [Credentials({})],
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if(passwordsMatch) return user
+        }
+        console.log('invalid credentials');
+        return null;
+      },
+    }),
+  ],
 });
-//? The whole purpose of this code is fancy way of saying only you friends get come inside to the party 
+//? The whole purpose of this ;code is fancy way of saying only you friends get come inside to the party
